61d85ad13d73aa20960cf03a97484a73556488a6
SanjayBoricha <sanjayboricha1001@gmail.com>
2019-10-09 11:57:59 +0530

    init

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout';
+import UserInfo from '../components/UserInfo';
+
+const Index = () => {
+  return (
+    <Layout>
+      <h1>Cool App</h1>
+      <UserInfo />
+    </Layout>
+  );
+};
+
+export default Index;
